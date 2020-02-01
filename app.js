834ba7aaa25c9084ed534fc3994903037a63d9c7
Pete Fitton <petefitton@Pete-Fittons-MacBook.local>
2020-01-31 18:56:29 -0800

    Quick updates

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// how do I want to handle animations?
-//     static animation vs battle action animation
-//     static = probably JS with some random activity that bounds back to center fairly quickly (small movements and at random intervals)
-//     battle action = probably JS too
 // DOM References that I use frequently---------------------------------------------------------------------------
 
 
@@ -18,11 +14,25 @@
 
 
 // functions--------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------
+
+
+
+
+// functions for character creation--------------------------------------------------------------------------------------------
 
 // create protagonist object function
 // create enemy1 object function
     // create enemy2 object function
     // create enemy3 object function
+
+
+    
+
+
+// functions for attacks/actions------------------------------------------------------------------------------------------------------
+
 
 // individual functions for each attack/action
     // should turn off static animation functions -----> staticAnimOff()
@@ -30,23 +40,43 @@
 
 
 
+
+
+// functions for moving between different screens--------------------------------------------------------------------------------------------
+
+
 // function which hides all divs for going between each screen/scene
 
-// function for showing all divs for title screen/scene
-// function for showing all divs for main menu screen/scene
+// function for showing all divs for title screen
+    // the addEventListener for the click anywhere function on title screen  - should include clicks as well as touches on mobile
+        // start playing audio and
+        // should clear all divs function
+        // removes the addeventlistener for click anywhere function
+        // should run main menu screen start
+// function for showing all divs for main menu screen
+    // the addEventListener for the game start button  - should include clicks as well as touches on mobile
+        // should clear all divs
+        // and start opening story function
+    // addevent listener for option button
+        // should run function for going to option screen
 // function for showing all divs for options screen/scene
     //this function shouldn't erase hide background as it should be seamless (should not run hide all divs)
     // should just hide necessary info from main menu screen
     // and unhide necessary info for options screen
-    // remove event listeners for start game and options buttons
-    // add event listener to return button
+    // remove event listeners for start game and options buttons - should include clicks as well as touches on mobile
+    // add event listener to return button - should include clicks as well as touches on mobile
+        // when return button is clicked or touched, the event listener for return button should go away
+        // should run function for showing all divs for main menu screen
 // return button function should do opposite of the above
     //this function shouldn't erase hide background as it should be seamless (should not run hide all divs)
     // should just hide necessary info from options screen
     // and unhide necessary info for main menu screen
-    // remove event listener to return button
-    // add event listeners for start game and options buttons
-// function for showing all divs for 1st battle screen/scene
+    // remove event listener to return button - should include clicks as well as touches on mobile
+    // add event listeners for start game and options buttons - should include clicks as well as touches on mobile
+// function for showing all divs for opening story screen
+    // not sure what divs should be here at the moment
+    // will want to
+// function for showing all divs for 1st battle screen
     // increments battleCounter by one
     // 3 battles should call the static animation functions
     // don't know what 3 battles above means
@@ -70,7 +100,29 @@
     // if level of hero is == 3, then show all five moves
     // start gameloop function
 // function for showing all divs for credits screen/scene
+    // once credits are fully displayed
+        // addeventlistener click anywhere to Reset
+            // when clicked/touched: the eventlistener removes itself
+            // and runs gameReset function
+// function for gameReset which executes once the game finishes and starts the game over without having to reload the page
+    // all things should be initialized including the hero and all enemies
+    // destroyed/removed as necessary
 
+
+// functions for animations--------------------------------------------------------------------------------------------
+
+// how do I want to handle animations?
+//     static animation vs battle action animation
+//     static = probably JS with some random activity that bounds back to center fairly quickly (small movements and at random intervals)
+//     battle action = probably JS too
+
+
+// I need to think a bit about how I want to initiate static animations
+    // need to place pseudocode lines of where that would go
+    // need to figure out areas and place pseudocode for areas where I need to turn off static animation
+        // static should turn off when battle animation is initiated
+            // and return on after battle animation is over
+        // static should turn off when the battle ends (maybe when level up is run?) or maybe put that inside the gameloop when the battle ends
 
 
 // static animation function during battles on a set interval
@@ -85,8 +137,10 @@
 // I will handle animations for line getting smaller with changes in the images, meaning that the action for reducing SP will call this function
 
 
-// function for going to option screen
-// function for going back from option screen to the main menu screen
+
+
+// function for leveling up--------------------------------------------------------------------------------------------
+
 
 // function for leveling up which includes learning new moves
     // end of 1st and 2nd battles should run level up function according to battleCounter variable
@@ -95,11 +149,14 @@
     // just checks to see who the current battleCounter variable is at and then levels up accordingly
     // if it's battle 1, then destroy enemy1's object and then run level up sequence
     // level up sequence function should pop up a div which says what move or moves you have learned
-    // addeventlistener for confirming you have read the text which then calls:
+    // addeventlistener for confirming you have read the text which then calls:  - should include clicks as well as touches on mobile
         // clear all divs function
         // show all divs for battle 2 or 3 function according to what the current battle count is
             // if I added additional story or world map, this section would change ^^^^^^
-        // it then removes itself (the eventlistener for confirming the text was read from leveling up)
+        // it then removes itself (the eventlistener for confirming the text was read from leveling up)  - should include clicks as well as touches on mobile
+
+
+// functions misc --------------------------------------------------------------------------------------------
 
 
 // gameloop function during battle that detects if enemy is no longer alive
@@ -107,18 +164,7 @@
     // when dead, the setInterval stops and then runs level up function
     // I think it might be called breakInterval??
 
-// function for gameReset which executes once the game finishes and starts the game over without having to reload the page
-    // all things should be initialized including the hero and all enemies
-    // destroyed/removed as necessary
 
-// title screen click anywhere function will
-    // start playing audio and
-    // run the hide all divs function
-    // and run main menu show divs function
 
 // DOMContentLoaded function
-// these might be put into a different function since the game will start with the title screen rather than the main menu
-    // the addEventListener for the game start button
-    // the addEventListener for the options button
-// so likely the title screen load page function will run
-    // the addEventListener for the click anywhere function on title screen
+    // so likely the title screen load page function will run
